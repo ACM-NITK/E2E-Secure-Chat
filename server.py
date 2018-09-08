4c334d640e522036fa771d860521de3ef3e65eb5
NishanthSubramanian <nishanth80442@gmail.com>
2018-09-08 14:32:21 +0530

    Routes added

---
+++ server.py
@@ -0,0 +1,15 @@
+from flask import Flask,render_template, request, redirect, url_for
+
+app=Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('login.html')
+
+@app.route('/chat')
+def chat():
+    return render_template('chat.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)

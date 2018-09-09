7710ddb13b5e877b903c52ad7ac52204f0ac9164
NishanthSubramanian <nishanth80442@gmail.com>
2018-09-09 20:56:53 +0530

    Created chat page and added sockets

--- server.py
+++ server.py
@@ -1,27 +1,21 @@
-
-# from flask import Flask, render_template
-# from flask_socketio import SocketIO
-
-# app = Flask(__name__)
-# socketio = SocketIO(app)
-
-# @app.route('/')
-# def login():
-#     return render_template('login.html')
-
-
-# if __name__ == '__main__':
-#     socketio.run(app, debug=True)
-
-
-
-
-
-from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask import Flask,render_template, request, redirect, url_for
+from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret"
 socketio = SocketIO(app)
 
+@socketio.on('message')
+def handlemessage(msg):
+    send( msg ,broadcast=True)
+
+@app.route('/')
+def index():
+    return render_template('login.html')
+
+@app.route('/chat')
+def chat():
+    return render_template('chat.html')
+
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, debug=True)

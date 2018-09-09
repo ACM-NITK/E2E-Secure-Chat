6ea9efd72519b57590d2db8a28c95760dbfdadaa
NishanthSubramanian <nishanth80442@gmail.com>
2018-09-09 17:59:19 +0530

    socketio

--- server.py
+++ server.py
@@ -1,14 +1,27 @@
 
+# from flask import Flask, render_template
+# from flask_socketio import SocketIO
+
+# app = Flask(__name__)
+# socketio = SocketIO(app)
+
+# @app.route('/')
+# def login():
+#     return render_template('login.html')
+
+
+# if __name__ == '__main__':
+#     socketio.run(app, debug=True)
+
+
+
+
+
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-@app.route('/')
-def login():
-    return render_template('login.html')
-
-
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app)

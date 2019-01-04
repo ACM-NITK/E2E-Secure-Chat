a3167cde63dc4163af3073f7c7aa741351d3c35b
tejasc256 <tejasc256@gmail.com>
2019-01-04 17:38:35 +0530

    Certificate Generation

--- server.py
+++ server.py
@@ -1,12 +1,35 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_socketio import SocketIO, send, emit
-from werkzeug import secure_filename
+import werkzeug.serving
+import ssl
+import OpenSSL
 
 app = Flask(__name__)
 app.config.from_object('config.Config')
 socketio = SocketIO(app)
 
+class PeerCertWSGIRequestHandler( werkzeug.serving.WSGIRequestHandler ):
+    def make_environ(self):
+        environ = super(PeerCertWSGIRequestHandler, self).make_environ()
+        x509_binary = self.connection.getpeercert(True)
+        x509 = OpenSSL.crypto.load_certificate( OpenSSL.crypto.FILETYPE_ASN1, x509_binary )
+        environ['peercert'] = x509
+        return environ
+
 users = {}
+certificates = {}
+
+app_key = './server.key'
+app_key_password = None
+app_cert = './server.pem'
+
+ca_cert = 'ca.pem'
+
+ssl_context = ssl.create_default_context( purpose=ssl.Purpose.CLIENT_AUTH,
+                                          cafile=ca_cert )
+ssl_context.load_cert_chain( certfile=app_cert, keyfile=app_key, password=app_key_password )
+ssl_context.verify_mode = ssl.CERT_REQUIRED
+
 @socketio.on('message')
 def handlemessage( json ):
     print('message received '+ str( json ))
@@ -14,6 +37,7 @@ def handlemessage( json ):
 
 @app.route('/')
 def index():
+    #certificates['1'] = request.environ['peercert'] #to store the certificates in the dictionary
     return render_template('login.html')
 
 @app.route('/chat')
@@ -36,17 +60,7 @@ def private_message(payload):
 
     emit('new_private_message', message, room=recipient_session_id)
 
-@app.route('/upload')
-def upload_file():
-   return render_template('upload.html')
-	
-@app.route('/uploader', methods = ['GET', 'POST'])
-def uploader_file():
-   if request.method == 'POST':
-      f = request.files['file']
-      f.save(secure_filename(f.filename))  #saves the file 
-      print('file saved')
-      return redirect(url_for('chat'))  #socketio.emit('uploadfile', f) wont work
-
 if __name__ == '__main__':
-    socketio.run(app)
+   app.run( ssl_context=ssl_context, request_handler=PeerCertWSGIRequestHandler )
+
+ 
